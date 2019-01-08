c977d074c812183e6f8b35bf0958368d8025f3f5
Vince Andrew Pimentel <contact@vincep.me>
2019-01-08 08:44:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get "/" do
+  get "/piglatinize" do
     erb :user_input
   end
 
-  post "/" do
+  post "/piglatinize" do
     @word = PigLatinizer.new(params[:text])
 
     erb :pigify
